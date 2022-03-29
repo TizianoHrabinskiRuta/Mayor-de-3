b845725d612ff55eecd1e0605c9d209e0a72aefa
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-03-29 13:45:20 -0300

    Ejercicio2

---
+++ Assets/ScriptsFolder/Ejercicio2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 4;
+        int Num2 = 5;
+
+        Debug.Log(num1 + " + " + Num2 + " = " + num1 + Num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
