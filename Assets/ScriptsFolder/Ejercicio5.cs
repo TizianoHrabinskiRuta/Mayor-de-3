c7f1bb14f7dc3693211b03122e5b87b5d132938b
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-03-29 13:57:21 -0300

    Ejercicio 5

---
+++ Assets/ScriptsFolder/Ejercicio5.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == 0 || num2 == 0)
+        {
+            Debug.Log("No se puede dividir por 0");
+            return;
+        }
+
+        Debug.Log(num1 + "/ " +num2 + " = " + num1 / num2 );
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
