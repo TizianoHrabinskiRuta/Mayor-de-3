8c1093eaceb7780fc623f0510c13843bfd0fa924
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-03-29 13:46:57 -0300

    Ejercicio3

--- Assets/ScriptsFolder/Ejercicio3.cs
+++ Assets/ScriptsFolder/Ejercicio3.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Ejercicio3 : MonoBehaviour
 {
+
+    public string Mensaje;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hola, " + Mensaje);
     }
 
     // Update is called once per frame
