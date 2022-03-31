7432daad91b851b786d7bb968204054614a94a31
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-03-31 15:41:26 -0300

    Ejercicio 12

---
+++ Assets/ScriptsFolder/Ejercicio12.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio12 : MonoBehaviour
+{
+    public int Dice1;
+    public int Dice2;
+    public int Dice3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int SumOfDice = Dice1 + Dice2 + Dice3;
+
+        switch (SumOfDice)
+        {
+            case 18:
+                Debug.Log("Excelente");
+                break;
+
+            case 12:
+                Debug.Log("Muy bien");
+                break;
+
+            case 6:
+                Debug.Log("Regular");
+                break;
+
+            default:
+                Debug.Log("Insuficiente");
+                break;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
