8bf0d31a4023d7cc9a537899f90c29206cb12e58
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-04-05 14:23:25 -0300

    EJ 15

---
+++ Assets/ScriptsFolder/Ejercicio15.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio15 : MonoBehaviour
+{
+
+
+    public int Input;
+    bool EstaEnA;
+    bool EstaEnB;
+    bool EstaEnAmbos;
+    bool NoEstaEnNingunos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Input % 2 != 0 && Input < 10)
+        {
+            EstaEnAmbos = true;
+            break;
+        }
+        else if(Input % 2 == 0 && Input < 10)
+        {
+            EstaEnB = true;
+            break;
+        }
+        else if(Input % 2 !=0 && Input >= 10)
+        {
+            EstaEnA = true;
+            break;
+        }
+        else if(Input % 2 == 0 && Input >= 10)
+        {
+            NoEstaEnNingunos = true;
+            break;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
