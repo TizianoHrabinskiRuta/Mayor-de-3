26ab39fd1db0d3e1500ed55f86dabbd8267fd29d
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-04-05 13:57:30 -0300

    Ej 11 y 13

--- Assets/ScriptsFolder/Ejercicio11.cs
+++ Assets/ScriptsFolder/Ejercicio11.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Ejercicio11 : MonoBehaviour
 {
+
+    public string Fecha;
+    public string Comprador;
+    public string Producto;
+    public string Cantidad;
+    public string Precio;
+    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Fecha de compra: " + Fecha + " \n Nombre del Comprador: " + Comprador + " \n Producto Solicitado: " + Producto + " \n Cantidad Solicitada: " + Cantidad + " \n Precio unitario: " + Precio + " \n Total a pagar: " + Convert.ToInt32(Precio) * Convert.ToInt32(Cantidad));
     }
 
     // Update is called once per frame
