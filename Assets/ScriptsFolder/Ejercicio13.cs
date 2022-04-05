bc40dcc91b48e51d0c03589a1ce6b79a5dcab240
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-04-05 14:01:25 -0300

    Ejercicio 13

--- Assets/ScriptsFolder/Ejercicio13.cs
+++ Assets/ScriptsFolder/Ejercicio13.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Ejercicio13 : MonoBehaviour
 {
+
+    public int HorasTrabajadas;
+    int Const = 16;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if( HorasTrabajadas <= 40)
+        {
+            Debug.Log("Tendria que cobrar: " + HorasTrabajadas * Const);
+            return;
+        }
+
+        Debug.Log("Tendria que cobrar: "((HorasTrabajadas - 40) * 20) + (HorasTrabajadas * Const));
     }
 
     // Update is called once per frame
